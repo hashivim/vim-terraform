b16e64ba40600a84fc2fa31a208b7386a0ea0332
Phil Porada <philporada@gmail.com>
2017-09-12 23:12:58 -0400

    Update provider syntax for newest version of Terraform

---
+++ update_provider_syntax.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# AUTHOR: Phil Porada - philporada@gmail.com
+# TICKET: https://github.com/hashivim/vim-terraform/issues/40
+# WHAT: As of Terraform 0.10.0, Hashicorp split out each provider into its own
+#       separate terraform-provider-* project. As a result, we have to hunt
+#       for all of the resources that each provider provides. This is a PITA
+#       but I guess good for Terraform. ¯\_(ツ)_/¯
+
+command -v jq >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "You need to install jq. Exiting..."
+    exit 1
+fi
+
+mkdir -p terraform-providers
+cd terraform-providers
+for i in $(curl -sL https://api.github.com/users/terraform-providers/repos?per_page=200 | jq -r .[].name); do
+    if [ ! -d $i ]; then
+	    git clone https://github.com/terraform-providers/$i
+    else
+        cd $i
+        git pull https://github.com/terraform-providers/$i
+        cd
+    fi
+done
+
+cd terraform-providers
+egrep -ro "\"(.*)\":.*resource.*\(\),$" | awk -F':' '{print $2}' | sed 's/"//g' | sort -u
+cd
